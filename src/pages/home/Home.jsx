bfcbb3124cf44fcf93c94cb55196241013cc87f5
hasan <mmhotmail>
2023-12-04 16:06:21 +0300

    home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <main className="text-white justify-content-center d-flex align-items-center">
+   <h1 className="mb-5">The Taste in the Middle of Europe</h1>
+    </main>
+  )
+}
+
+export default Home
