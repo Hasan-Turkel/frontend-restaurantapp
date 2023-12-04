9c4e230b23538583f29be2f26a8a042e6363c414
hasan <mmhotmail>
2023-12-04 14:15:21 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg bg-black">
+    <div className="container-fluid align-items-end">
+      <NavLink className="navbar-brand text-white fs-2 " to="/">
+        Turkel's Restaurant
+      </NavLink>
+      <button
+        className="navbar-toggler bg-white"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>     
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav w-100 px-5">
+            
+            <NavLink className="nav-link text-white fs-4" aria-current="page" to="/restaurants">
+            Our Restaurants 
+          </NavLink> 
+          <NavLink className="nav-link text-white fs-4" to="/create-reservation">
+            Create Reservation 
+          </NavLink>
+          <NavLink className="nav-link text-white fs-4" to="/my-reservations">
+            My Reservations
+          </NavLink>         
+          <NavLink className="nav-link text-white fs-4 " to="/login">
+            Login
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+  
   )
 }
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Restaurants from "../pages/Restaurants";
+import Reservation from "../pages/Reservation";
+import Login from "../pages/Login";
+import MyReservations from "../pages/MyReservations";
 
 const AppRouter = () => {
   return (
@@ -9,6 +13,10 @@ const AppRouter = () => {
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/restaurants" element={<Restaurants />} />
+      <Route path="/create-reservation" element={<Reservation />} />
+      <Route path="/my-reservations" element={<MyReservations />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
     <Footer />
   </Router>
